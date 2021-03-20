e1bcafc299f83ef2d8611861995b0ab93ad3d300
Filipe <filipe_oli@hotmail.com>
2021-03-20 11:58:38 -0300

    Teste no console

---
+++ js/controller.js
@@ -0,0 +1,8 @@
+var nome = "Renato Oliveira";
+var peso = 76.00;
+var altura = 1.70;
+var imc = peso / (altura ** 2);
+
+console.log(nome);
+console.log(`Peso: ${peso} - Altura: ${altura}`);
+console.log(`Calculo do IMC: ${imc}`);
