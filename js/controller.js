5f411f9db066e4fa034a42ccb6b74d504be6d8de
Filipe <filipe_oli@hotmail.com>
2021-03-20 12:32:30 -0300

    Alteração da exibição dos valores

--- js/controller.js
+++ js/controller.js
@@ -1,8 +1,22 @@
 var nome = "Renato Oliveira";
 var peso = 76.00;
-var altura = 1.70;
-var imc = peso / (altura ** 2);
+var altura = 1.78;
+var imc = (peso / (altura ** 2)).toFixed(2);
 
+/*
 console.log(nome);
 console.log(`Peso: ${peso} - Altura: ${altura}`);
-console.log(`Calculo do IMC: ${imc}`);
+console.log(`Cálculo do IMC: ${imc}`); //Limita para duas casas depois da virgula.
+*/
+
+var elNome = document.getElementById("nome");
+elNome.innerHTML = nome;
+
+var elPeso = document.getElementById("peso");
+elPeso.innerHTML = peso;
+
+var elAltura = document.getElementById("altura");
+elAltura.innerHTML = altura;
+
+var elImc = document.getElementById("imc");
+elImc.innerHTML = imc;
