05d6f37fbebdbf3a5848f15c19ec1bbb3a7bdaf5
Filipe <filipe_oli@hotmail.com>
2021-03-20 23:46:53 -0300

    Adição das funções e refatoramento do código

--- js/controller.js
+++ js/controller.js
@@ -1,39 +1,9 @@
 let nome = "Renato Oliveira";
 let peso = 76.00;
 let altura = 1.78;
-let imc = (peso / (altura ** 2)).toFixed(2);
-let situacao = null;
-
+mostraPaciente(nome, peso, altura);
 /*
 console.log(nome);
 console.log(`Peso: ${peso} - Altura: ${altura}`);
 console.log(`Cálculo do IMC: ${imc}`); //Limita para duas casas depois da vírgula.
 */
-
-if (imc < 18.5) {
-    situacao = "Magreza";
-}
-else if (imc <= 24.9) {
-    situacao = "Normal";
-}
-else if (imc <= 30) {
-    situacao = "Sobrepeso";
-}
-else {
-    situacao = "Obesidade";
-}
-
-let elNome = document.getElementById("nome");
-elNome.innerHTML = nome;
-
-let elPeso = document.getElementById("peso");
-elPeso.innerHTML = `${peso} kg`;
-
-let elAltura = document.getElementById("altura");
-elAltura.innerHTML = `${altura.toString().replace(".", ",")} m`;
-
-let elImc = document.getElementById("imc");
-elImc.innerHTML = imc.replace(".", ",");
-
-let elSituacao = document.getElementById("situacao");
-elSituacao.innerHTML = situacao;
---
+++ js/funcoes.js
@@ -0,0 +1,40 @@
+function calculaIMC(peso, altura) {
+    return (peso / altura ** 2);
+};
+
+function verificaIMC(imc) {
+    let situacao = null;
+    if (imc < 18.5) {
+        situacao = "Magreza";
+    }
+    else if (imc <= 24.9) {
+        situacao = "Normal";
+    }
+    else if (imc <= 30) {
+        situacao = "Sobrepeso";
+    }
+    else {
+        situacao = "Obesidade";
+    }
+    return situacao;
+};
+
+function mostraPaciente(nome, peso, altura) {
+    let imc = calculaIMC(peso, altura);
+    let situacao = verificaIMC(imc);
+
+    let elNome = document.getElementById("nome");
+    elNome.innerHTML = nome;
+
+    let elPeso = document.getElementById("peso");
+    elPeso.innerHTML = `${peso} kg`;
+
+    let elAltura = document.getElementById("altura");
+    elAltura.innerHTML = `${altura.toString().replace(".", ",")} m`;
+
+    let elImc = document.getElementById("imc");
+    elImc.innerHTML = imc.replace(".", ",");
+
+    let elSituacao = document.getElementById("situacao");
+    elSituacao.innerHTML = situacao;
+};
