a605e61f47a0c9964ba6d557afec4a138e442a57
Filipe <filipe_oli@hotmail.com>
2021-03-21 01:14:57 -0300

    Adição do style.css

--- js/controller.js
+++ js/controller.js
@@ -1,10 +1,21 @@
-let paciente = {
-    nome: "Renato Oliveira",
-    peso: 76.00,
-    altura: 1.78
-};
+let pacientes = [];
+const botaoCalcular = document.getElementById("calcular");
+botaoCalcular.addEventListener("click", function () {
+    try {
+        var paciente = lePaciente();
+    } catch (error) {
+        alert(error);
+        return;
+    }
+    pacientes.push(paciente);   // Adiciona paciente em memória.
+    mostraPaciente(paciente);
+});
 
-mostraPaciente(paciente);
+const botaoRelatorio = document.getElementById("relatorio");
+botaoRelatorio.addEventListener("click", function () {
+    geraRelatorio(pacientes);
+});
+
 /*
 console.log(nome);
 console.log(`Peso: ${peso} - Altura: ${altura}`);
--- js/funcoes.js
+++ js/funcoes.js
@@ -23,18 +23,47 @@ function mostraPaciente(paciente) {
     let imc = calculaIMC(paciente.peso, paciente.altura);
     let situacao = verificaIMC(imc);
 
-    let elNome = document.getElementById("nome");
+    const elNome = document.getElementById("nome");
     elNome.innerHTML = paciente.nome;
 
-    let elPeso = document.getElementById("peso");
+    const elPeso = document.getElementById("peso");
     elPeso.innerHTML = `${paciente.peso} kg`;
 
-    let elAltura = document.getElementById("altura");
+    const elAltura = document.getElementById("altura");
     elAltura.innerHTML = `${paciente.altura.toString().replace(".", ",")} m`;
 
-    let elImc = document.getElementById("imc");
+    const elImc = document.getElementById("imc");
     elImc.innerHTML = imc.toString().replace(".", ",");
 
     let elSituacao = document.getElementById("situacao");
     elSituacao.innerHTML = situacao;
+};
+
+function lePaciente() {
+    const inputNome = document.getElementById("txtNome");
+    const inputPeso = document.getElementById("txtPeso");
+    const inputAltura = document.getElementById("txtAltura");
+
+    if (inputPeso.value == "" || inputAltura.value == "") { // Impede entrada de dados inválidos.
+        throw "Favor preencha os dados corretamente!";
+    }
+
+    let paciente = {
+        nome: inputNome.value,
+        peso: inputPeso.value,
+        altura: inputAltura.value
+    };
+
+    return paciente;
+};
+
+function geraRelatorio(pacientes) {
+    const listaNumerada = document.getElementById("listaNumerada");
+    listaNumerada.innerHTML = null; // Limpa lista.
+
+    pacientes.forEach(paciente => {
+        let imc = calculaIMC(paciente.peso, paciente.altura);
+        let situacao = verificaIMC(imc);
+        listaNumerada.innerHTML += `<li>${paciente.nome} - Situação: ${situacao}</li>`;
+    });
 };
