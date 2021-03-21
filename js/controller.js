a90c4a74c33797a362c841318f8bd42257415145
Filipe <filipe_oli@hotmail.com>
2021-03-20 23:29:40 -0300

    Código refatorado

--- js/controller.js
+++ js/controller.js
@@ -7,7 +7,7 @@ let situacao = null;
 /*
 console.log(nome);
 console.log(`Peso: ${peso} - Altura: ${altura}`);
-console.log(`Cálculo do IMC: ${imc}`); //Limita para duas casas depois da virgula.
+console.log(`Cálculo do IMC: ${imc}`); //Limita para duas casas depois da vírgula.
 */
 
 if (imc < 18.5) {
@@ -17,7 +17,7 @@ else if (imc <= 24.9) {
     situacao = "Normal";
 }
 else if (imc <= 30) {
-    situacao = "Sobrpeso";
+    situacao = "Sobrepeso";
 }
 else {
     situacao = "Obesidade";
@@ -27,10 +27,10 @@ let elNome = document.getElementById("nome");
 elNome.innerHTML = nome;
 
 let elPeso = document.getElementById("peso");
-elPeso.innerHTML = peso;
+elPeso.innerHTML = `${peso} kg`;
 
 let elAltura = document.getElementById("altura");
-elAltura.innerHTML = altura.toString().replace(".", ",");
+elAltura.innerHTML = `${altura.toString().replace(".", ",")} m`;
 
 let elImc = document.getElementById("imc");
 elImc.innerHTML = imc.replace(".", ",");
